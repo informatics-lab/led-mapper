0ea6a3c645e3c2453f30608a18ba1d526df3197c
Niall Robinson <niall.h.robinson@gmail.com>
2016-03-18 15:27:24 +0000

    working with phone

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+var nlights = 1;
+var imgres = [500, 375];
+
+
 var video = document.querySelector("#videoElement");
 
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
@@ -17,7 +21,6 @@ function videoError(e) {
 function draw(v,c,w,h) {
     if(v.paused || v.ended) return false;
     c.drawImage(v,0,0,w,h);
-    setTimeout(draw,1000,v,c,w,h);
 }
 
 document.addEventListener('DOMContentLoaded', function(){
@@ -31,8 +34,17 @@ document.addEventListener('DOMContentLoaded', function(){
   canvas.height = ch;
 
   v.addEventListener('play', function(){
-      draw(this,context,cw,ch);
-      calibrate(nlights, imgres)
+      var that = this;
+      setTimeout(function(){
+        draw(that,context,cw,ch);
+        var lightonrgba = getImgRGBA(imgres[0], imgres[1]);
+        console.log("Shift!")
+        setTimeout(function(){
+            draw(that,context,cw,ch);
+            calibrate(nlights, imgres, lightonrgba);
+        }, 2000)
+      }, 2000)
+
   },false);
 
 },false);
@@ -42,6 +54,13 @@ function getImgRGBA(np, nq){
   var context = canvas.getContext('2d');
 
   return context.getImageData(0, 0, np, nq).data;
+}
+
+function drawRect(x, y){
+  var canvas = document.getElementById('canvasElement');
+  var context = canvas.getContext('2d');
+  context.fillStyle = "red";
+  context.fillRect(x, y, 10, 10);
 }
 
 function rgbaToPQI(rgba, np, nq){
@@ -61,43 +80,48 @@ function rgbaToPQI(rgba, np, nq){
 }
 
 
-function calibrate(nlights, imgres){
-def calibrate(nlights, imgres):
+function calibrate(nlights, imgres, lightonrgba){
   var white = "#ffffff", black = "#000000";
   var lighton = [white]; for (var i=0; i < (nlights-1); i++){lighton.push(black)};
   var lightoff = []; for (var i=0; i < (nlights); i++){lighton.push(black)};
   var positions = [];
 
-  var minx = 0, miny = 0, maxx = 100000000000, maxy = 100000000000;
+  var minx = 100000000000, miny = 100000000000, maxx = 0, maxy = 0;
 
-  for (var n=0; n<nlights.length; n++){
-    console.log(".");
+  for (var n=0; n<nlights; n++){
+    // console.log(".");
     // requests.post(lighton)
-    lightonrgba = getImgRGBA(imgres[0], imgres[1]);
-    
+    //lightonrgba = getImgRGBA(imgres[0], imgres[1]);
+
     // requests.post(lightsoff)
     lightoffrgba = getImgRGBA(imgres[0], imgres[1]);
 
     maxdiff = 0;
     maxpos = 0;
-    for (var i=0; i<lightonrgba.length; i+4){
-      var diff = ((lightonrgba[i]-lightoffrgba[i]) + (lightonrgba[i+1]-lightoffrgba[i+1]) + (lightonrgba[i+2]-lightoffrgba[i+2]))/3.0;
+    for (var i=0; i<lightonrgba.length; i+=4){
+      var onlum = (0.21*lightonrgba[i]) + (lightonrgba[i+1]*0.71) + (lightonrgba[i+2]*0.07)
+      var offlum = (0.21*lightoffrgba[i]) + (lightoffrgba[i+1]*0.71) + (lightoffrgba[i+2]*0.07)
+      var diff = onlum - offlum;
       if (diff > maxdiff) {maxdiff=diff; maxpos=i/4;}
     }
 
-    var p = Math.floor(maxpos/np);
-    var q = maxpos%np;
+    var p = maxpos%imgres[0];
+    var q = Math.floor(maxpos/imgres[0]);
 
     if (p > maxx){maxx = p;}
     if (p < minx){minx = p;}
     if (q > maxy){maxy = q;}
-    if (q < maxy){maxy = q;}
-
+    if (q < miny){miny = q;}
     positions.push([p,q])
     
+    console.log(positions)
+    drawRect(p,q);
+
     lighton.unshift(black)
     lighton.pop()
   }
+
+  console.log(positions)
 
   var normpositions = [];
   var imgpositions = [];
