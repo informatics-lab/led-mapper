0085064f2757a7f1dc62dc272668602afb801a06
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-03-18 13:19:49 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+var video = document.querySelector("#videoElement");
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+
+if (navigator.getUserMedia) {
+   navigator.getUserMedia({video: true}, handleVideo, videoError);
+}
+
+function handleVideo(stream) {
+   video.src = window.URL.createObjectURL(stream);
+}
+
+function videoError(e) {
+   // do something
+}
+
+function draw(v,c,w,h) {
+    if(v.paused || v.ended) return false;
+    c.drawImage(v,0,0,w,h);
+    setTimeout(draw,1000,v,c,w,h);
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+  var v = document.getElementById('videoElement');
+  var canvas = document.getElementById('canvasElement');
+  var context = canvas.getContext('2d');
+
+  var cw = Math.floor(canvas.clientWidth );
+  var ch = Math.floor(canvas.clientHeight );
+  canvas.width = cw;
+  canvas.height = ch;
+
+  v.addEventListener('play', function(){
+      draw(this,context,cw,ch);
+  },false);
+
+},false);
